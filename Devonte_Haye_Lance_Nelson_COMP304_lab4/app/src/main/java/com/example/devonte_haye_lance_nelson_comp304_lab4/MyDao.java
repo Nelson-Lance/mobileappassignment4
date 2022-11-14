42a00d05df6ef61e7976166dbed255fc553d63a0
Lance Nelson <lance.nelson@hotmail.com>
2022-11-14 16:40:08 -0500

    added view test info and modified database objects to interact with view test info

--- Devonte_Haye_Lance_Nelson_COMP304_lab4/app/src/main/java/com/example/devonte_haye_lance_nelson_comp304_lab4/MyDao.java
+++ Devonte_Haye_Lance_Nelson_COMP304_lab4/app/src/main/java/com/example/devonte_haye_lance_nelson_comp304_lab4/MyDao.java
@@ -2,6 +2,8 @@ package com.example.devonte_haye_lance_nelson_comp304_lab4;
 
 import androidx.room.Dao;
 import androidx.room.Insert;
+import androidx.room.Query;
+import java.util.List;
 
 @Dao
 public interface MyDao {
@@ -9,5 +11,6 @@ public interface MyDao {
     @Insert
     public void addTest(Test test);
 
-
+    @Query ("select * from test")
+    public List<Test> getTest();
 }
---
+++ Devonte_Haye_Lance_Nelson_COMP304_lab4/app/src/main/java/com/example/devonte_haye_lance_nelson_comp304_lab4/ViewTestInfo.java
@@ -0,0 +1,58 @@
+package com.example.devonte_haye_lance_nelson_comp304_lab4;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.List;
+
+public class ViewTestInfo extends AppCompatActivity {
+
+    private TextView displayTest;
+
+    private Button displayButton;
+
+    public static MyDB myDB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate (savedInstanceState);
+        setContentView (R.layout.activity_view_test_info);
+
+        myDB = MyDBAccessor.getMyDBInstance (getApplicationContext ());
+
+        displayButton = findViewById (R.id.displayTest);
+        displayButton.setOnClickListener (new View.OnClickListener () {
+            @Override
+            public void onClick(View view) {
+
+                displayTest = findViewById (R.id.displaytestView);
+
+
+                List<Test> tests = ViewTestInfo.myDB.myDao ().getTest ();
+
+                String info = "";
+
+                for(Test test: tests) {
+                    int testapplicantid = test.getApplicantId ();
+                    int testexaminerid = test.getExaminerId ();
+                    String testresult = test.getTestResult ();
+                    int testdate = test.getTestDate ();
+                    int testroute = test.getTestRoute ();
+
+                    info = info +
+                            "\n\n Applicant Id: " + testapplicantid +
+                            "\n Examiner Id: " + testexaminerid +
+                            "\n Test Result: " + testresult +
+                            "\n Test Date: " + testdate +
+                            "\n Test Route #: " + testroute;
+                }
+                displayTest.setText (info);
+            }
+        });
+
+    }
+}
